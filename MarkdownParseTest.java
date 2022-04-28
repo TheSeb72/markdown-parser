4708f3d74cb8654dfef2b77a02632e15d3f941bd
TheSeb72 <90715607+TheSeb72@users.noreply.github.com>
2022-04-28 10:27:46 -0700

    Add files via upload

---
+++ MarkdownParseTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() {
+        Path path = Path.of("test-file.md");
+        String readString = Files.readString(path);
+        ArrayList<String> = links = MarkdownParse.getLinks(readString);
+        assertEquals(List.of("https://something.com", "some-page.html"), links);
+    }
+}
